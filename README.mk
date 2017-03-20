f2087258b1acde19cd05206f542d32fc05943fd6
Robert Foss <robert.foss@collabora.com>
2017-03-20 09:35:26 -0400

    Add README

---
+++ README.mk
@@ -0,0 +1,11 @@
+# Component Holder
+This is a simpe
+
+
+# Parts
+## Brass standoffs
+ * 5mm  female-male
+ * 10mm female-male
+ * 15mm fmeale-female
+##
+ * [Pill bottle](https://www.aliexpress.com/item/7-Pieces-Lot-3ML-Portable-Plastic-Pill-Bottle-Medicine-Case-For-Healthy-Care-Empty-Pill-Holders/32677858742.html)
